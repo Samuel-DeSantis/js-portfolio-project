84a23c23106c9bacf9a22bdd18cd71df52b4873f
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2022-01-01 21:33:36 -0700

    Boostrap UI built, working on JS

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -3,7 +3,29 @@ const climbForm = document.querySelector('.container')
 let addClimb = false
 let divCollect = document.querySelector('#climb-collection')
 
-function () {}
+function myFunc() {
+    let climbPost = 
+    "<div class='col'>" +
+       "<div class='card shadow-sm'>" +
+            "<svg class='bd-placeholder-img card-img-top' width='100%' height='225' xmlns='http://www.w3.org/2000/svg' role='img' aria-label='Placeholder: Thumbnail' preserveAspectRatio='xMidYMid slice' focusable='false'><title>Placeholder</title><rect width='100%' height='100%' fill='#55595c'/><text x='50%' y='50%' fill='#eceeef' dy='.3em'>Thumbnail</text></svg>" +
+            "<div class='card-body'>" +
+                "<p class='card-text'>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>" +
+                "<div class='d-flex justify-content-between align-items-center'>" +
+                    "<div class='btn-group'>" +
+                        "<button type='button' class='btn btn-sm btn-outline-secondary'>Edit</button>" +
+                    "</div>" +
+                    "<small class='text-muted'>9 mins</small>" +
+                "</div>" +
+            "</div>" +
+        "</div>" +
+    "</div>";
+
+    document.getElementById("myClimbs").innerHTML += climbPost;
+
+    // var para = document.createElement("P");
+    // para.innerText = "This is a paragraph.";
+    // document.getElementById("myClimbs").appendChild(para);
+}
 
 function getClimbs () {
     return fetch('http://localhost:3000/climbs')
@@ -15,16 +37,12 @@ function postClimb () {
 
 }
 
-function renderThumbnail () {
-    divThumbnail.setAttribute('class', 'bd-placeholder-img card-img-top')
-    divThumbnail.setAttribute('width', '100%')
-    divThumbnail.setAttribute('height', '225')
-    divThumbnail.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
-    divThumbnail.setAttribute('role', 'img')
-    divThumbnail.setAttribute('aria-label', 'Placeholder: Thumbnail')
-    divThumbnail.setAttribute('preserveAspectRatio', 'xMidYMid slice')
-    divThumbnail.setAttribute('focusable', 'false')
-    divThumbnail.setAttribute('focusable', 'false')
+function testRenderClimb () {
+    let divCol = document.createElement('div').setAttribute('class', 'col')
+    let divCardShadowSM = document.createElement('div').setAttribute('class', 'card shadow-sm')
+    let divThumbnail = document.createElement('div')
+    document.getElementById("myClimbs").appendChild(divCol);
+
 }
 
 function renderClimb () {
@@ -33,8 +51,6 @@ function renderClimb () {
     let divThumbnail = document.createElement('div')
     renderThumbnail()
 }
-
-
 
 addBtn.addEventListener('click', () => {
     addClimb = !addClimb
@@ -49,3 +65,14 @@ addBtn.addEventListener('click', () => {
     }
 })
 
+// function renderThumbnail () {
+//     divThumbnail.setAttribute('class', 'bd-placeholder-img card-img-top')
+//     divThumbnail.setAttribute('width', '100%')
+//     divThumbnail.setAttribute('height', '225')
+//     divThumbnail.setAttribute('xmlns', 'http://www.w3.org/2000/svg')
+//     divThumbnail.setAttribute('role', 'img')
+//     divThumbnail.setAttribute('aria-label', 'Placeholder: Thumbnail')
+//     divThumbnail.setAttribute('preserveAspectRatio', 'xMidYMid slice')
+//     divThumbnail.setAttribute('focusable', 'false')
+//     divThumbnail.setAttribute('focusable', 'false')
+// }

46b44f0fab35aa4754e3581e4303d3f84a0fd9e8
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2022-01-08 23:29:15 -0700

    Add Climbs now working, Edit is next

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,5 @@
 const CLIMBING_LOGGER_URL = "http://localhost:3000/climbs";
 const newClimbForm = document.getElementById('newClimb')
-let myData = 0
 
 document.addEventListener('DOMContentLoaded', () => {
     getClimbs()
@@ -8,32 +7,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
 newClimbForm.addEventListener('submit', addClimb)
 
-function getClimbs () {
-    return fetch(CLIMBING_LOGGER_URL)
-    .then(response => response.json())
-    .then(json => recordedClimb(json))
-    .catch(error => console.error(error))
-}
+function climbParams () {
+    const location = document.getElementById('location').value
+    const difficulty = document.getElementById('difficulty').value
+    const ascents = document.getElementById('ascents').value
+    const description = document.getElementById('description').value
 
-
-function addClimb () {
-    
     data = {
         climb: {
-            location: event.target.dataset.location,
-            difficulty: event.target.dataset.difficulty,
-            ascents: event.target.dataset.ascents,
-            description: event.target.dataset.description
+            location: location,
+            difficulty: difficulty,
+            ascents: ascents,
+            description: description
         }
     }
+    return data
+}
 
+async function getClimbs () {
+    try {
+        const response = await fetch(CLIMBING_LOGGER_URL);
+        const json = await response.json();
+        return recordedClimb(json);
+    } catch (error) {
+        return console.error(error);
+    }
+}
+
+function addClimb (event) {
+    console.log(climbParams)
     fetch(CLIMBING_LOGGER_URL, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json"
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(climbParams())
     })
     .then(response => response.json())
     .then(result => console.log('Success:', result))
@@ -50,10 +59,10 @@ function recordedClimb(data) {
         "<div class='col'>" +
            "<div class='card shadow-sm'>" +
                 "<div class='card-body'>" +
-                    `<p class='card-text'>Location: ${data[i].location}<br>Ascents: ${data[i].ascents}<br>Difficulty: ${data[i].difficulty}<br>Description: ${data[i].description}</p>` +
+                    `<p id="climbID" hidden>${data[i].id}</p><p class='card-text'>Location: ${data[i].location}<br>Ascents: ${data[i].ascents}<br>Difficulty: ${data[i].difficulty}<br>Description: ${data[i].description}</p>` +
                     "<div class='d-flex justify-content-between align-items-center'>" +
                         "<div class='btn-group'>" +
-                            "<button type='button' class='btn btn-sm btn-outline-secondary'>Edit</button>" +
+                            `<button type='button'  data-bs-toggle="modal" data-bs-target="#exampleModal" data-toggle="tooltip" data-placement="bottom" class='btn btn-sm btn-outline-secondary'>Edit</button>` +
                             `<button type='button' onclick='removeClimb(${data[i].id})' class='btn btn-sm btn-outline-secondary' data-toggle="tooltip" data-placement="bottom" title="Click to Remove this Climb">Remove</button>` +
                         "</div>" +
                     "</div>" +
@@ -65,16 +74,100 @@ function recordedClimb(data) {
     }
 }
 
-function removeClimb (id) {
-    return fetch(CLIMBING_LOGGER_URL+`/${id}`, {
+async function removeClimb (id) {
+    await fetch(CLIMBING_LOGGER_URL + `/${id}`, {
         method: "DELETE",
         headers: {
             "Content-Type": "application/json",
-        }}).then(()=>{window.location.reload()})
+        }
+    })
+    .then(result => console.log('Success:', result))
+    .then(error => console.log('Error:', error))
+    window.location.reload();
+}
+
+function findClimb (id) {
+    fetch(CLIMBING_LOGGER_URL + `/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(climbParams())
+    })    
+    .then(result => console.log('Success:', result))
+    .then(error => console.log('Error:', error))
+    window.location.reload();
+}
+
+function editClimb (id) {
+    editModal(findClimb(id))
+
+    fetch(CLIMBING_LOGGER_URL + `/${id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        }
+    })
+    .then(result => console.log('Success:', result))
+    .then(error => console.log('Error:', error))
+    window.location.reload();
 }
 
 
 
+function editModal (data) {
+
+    let editPost = 
+        `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">` +
+            `<div class="modal-dialog">` +
+            `<div class="modal-content">` +
+                `<div class="modal-header">` +
+                `<h5 class="modal-title" id="exampleModalLabel">Add a Climb</h5>` +
+                `<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>` +
+                `</div>` +
+                `<div class="modal-body">` +
+                `<form id="newClimb">` +
+                    `<div class="row g-3 align-items-center">` +
+                    `<div class="col-auto">` +
+                        `<label for="location" class="col-form-label">Location</label>` +
+                    `</div>` +
+                    `<div class="col-auto">` +
+                        `<input type="text" id="location" name="location" class="location" placeholder="${data.location}">` +
+                    `</div>` +
+                    `</div>` +
+                    `<div class="row g-3 align-items-center">` +
+                    `<div class="col-auto">` +
+                        `<label for="difficulty" class="col-form-label">Difficulty</label>` +
+                    `</div>` +
+                    `<div class="col-auto">` +
+                        `<input type="text" id="difficulty" name="difficulty" class="form-control" placeholder="${data.difficulty}">` +
+                    `</div>` +
+                    `</div>` +
+                    `<div class="row g-3 align-items-center">` +
+                    `<div class="col-auto">` +
+                        `<label for="ascents" class="col-form-label">Ascents</label>` +
+                    `</div>` +
+                    `<div class="col-auto">` +
+                        `<input type="text" id="ascents" name="ascents" class="form-control placeholder="${data.ascents}">` +
+                    `</div>` +
+                    `</div>` +
+                    `<br>` +
+                    `<div class="mb-3">` +
+                    `<label for="description" class="form-label">Description</label>` +
+                    `<input type="textarea" class="form-control" id="description" name="description" placeholder="${data.description}">` +
+                    `</div>` +
+                    `<br>` +
+                    `<div class="modal-footer">` +
+                    `<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>` +
+                    `<button type="submit" class="btn btn-primary">Submit</button>` +
+                    `</div>` +
+                `</form>` +
+                `</div>` +
+            `</div>` +
+            `</div>` +
+        `</div>`
+}
 
 
 
